0c28e55f438d163b7b0a8f8afc343341ac0f435d
Austin Williams <auswil13@gmail.com>
2020-01-23 20:03:20 -0600

    initial push

---
+++ Develop/server/routes/api/products.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const productsController = require("../../controllers/productsController");
+
+// Matches with "/api/products"
+router
+  .route("/")
+  .get(productsController.findAll)
+  .post(productsController.create);
+
+// Matches with "/api/products/:id"
+router
+  .route("/:id")
+  .get(productsController.findById)
+  .put(productsController.update)
+  .delete(productsController.remove);
+
+// Matches with "/api/products/category"
+router.route("/category")
+  .get(productsController.findByClass);
+
+router.route("/find/cart")
+  .get(productsController.find);
+
+module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bestBuy
+
+A marketplace that allows users to view products from Best Buy. The user will be able to view details about the product, and if they decide, add the product to a shopping cart.
+
+# User Story
+
+As a customer, I want to be able to search through Best Buy's directory and add items to my shopping cart. Once I'm finished shopping, I want to be able to navigate to my shopping cart and view a short summary of the items that I purchased.
