c3e42663db11f2696dfbe575c8415445699495c4
Austin Williams <auswil13@gmail.com>
2020-01-31 12:15:12 -0600

    push build, heroku deployment

--- Develop/server/models/product.js
+++ Develop/server/models/product.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  title: String,
-  seller: String,
-  description: String,
-  short_description: String,
-  thumbnail_url: String,
-  flash_day: String,
+  id: String,
+  name: String,
+  price: Number,
+  image: String,
   post_date: { type: Date, default: Date.now }
 });
 
